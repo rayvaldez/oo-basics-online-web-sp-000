0ada7d364d05596a99ce3d1017f2b7fd1e7c464a
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-09 17:06:02 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 class Shoe
-  attr_accessor :brand, :color, :size,: :material, :condition
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
 
   end
 
   def cobble
-    
+
   end
 end
